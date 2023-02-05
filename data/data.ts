f2894fbe554802d3a08ccba52ff1a9d31beb90d8
HartJN <jameshart04@gmail.com>
2023-02-06 09:52:46 +1300

    add projects data

---
+++ data/data.ts
@@ -0,0 +1,50 @@
+interface Project {
+  name: string
+  description: string
+  image: string
+  github: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Secret Santa',
+    description:
+      'A Secret Santa event creator built with React, allowing for hassle-free gifter assignments and invite sharing.',
+    image: '/elfco-secret-santa.png',
+    github: 'https://github.com/HartJN/elfco-secret-santa-only',
+    link: 'https://elfco-secret-santa.herokuapp.com',
+  },
+  {
+    name: 'Badger Match',
+    description:
+      "An animal companion finding application. We'll show you different candidates to help you decide which animal is your ideal match.",
+    image: '/badgermatch.png',
+    github: 'https://github.com/HartJN/BadgerMatch',
+    link: '#',
+  },
+  {
+    name: 'TechTok',
+    description:
+      'TechTok is a social media application that allows users to post and comment on tech-related content. Built using Typescript, Next.js, Sanity.io.',
+    image: '/tech-tok.png',
+    github: '#',
+    link: '#',
+  },
+  {
+    name: 'Forkify',
+    description:
+      'Forkify is a recipe search application that allows users to search for recipes and save them to their favorites. Built using OOP JavaScript and MVC architecture.',
+    image: '/forkify.png',
+    github: 'https://github.com/HartJN/Forkify',
+    link: '#',
+  },
+  {
+    name: 'Mapty',
+    description:
+      'Mapty is a workout tracking application that allows users to track their workouts and save them to their favorites. Built using OOP JavaScript and MVC architecture.',
+    image: '/mapty.png',
+    github: 'https://github.com/HartJN/Mapty',
+    link: '#',
+  },
+]
