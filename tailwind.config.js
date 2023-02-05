48c9370489548ceb83ce4a92d97aa6825463cd40
HartJN <jameshart04@gmail.com>
2023-02-06 10:04:10 +1300

    add page transition animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,10 @@ module.exports = {
         fadeIn: 'fadeIn 1.5s',
         bounce:
           'bounce 0.5s alternate cubic-bezier(0.95, 0.05, 0.795, 0.035) infinite',
-        slideUp: 'slideUp 0.5s',
-        slideUpEaseInOut: 'slideUp 0.5s ease-in-out',
-        slideUpCubiBezier: 'slideUp 1s cubic-bezier(0.165, 0.84, 0.44, 1)',
+        pageTransition: 'pageTransition 0.5s',
+        pageTransitionEaseInOut: 'pageTransition 0.5s ease-in-out',
+        pageTransitionCubiBezier:
+          'pageTransition 1s cubic-bezier(0.165, 0.84, 0.44, 1)',
       },
       animationDelay: {
         0: '0s',
@@ -35,7 +36,7 @@ module.exports = {
           from: { opacity: 0 },
           to: { opacity: 1 },
         },
-        slideUp: {
+        pageTransition: {
           from: { transform: 'translateY(100%)' },
           to: { transform: 'translateY(0)' },
         },
