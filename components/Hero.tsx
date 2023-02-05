006a2d05dadf221ae7f62f5ce7d2d75345f51d45
HartJN <jameshart04@gmail.com>
2023-02-06 09:46:42 +1300

    rename export

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { Link } from 'react-scroll/modules'
 import { HiArrowDown } from 'react-icons/hi'
 
-const HeroSection = () => {
+const Hero = () => {
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
@@ -60,4 +60,4 @@ const HeroSection = () => {
   )
 }
 
-export default HeroSection
+export default Hero
