bf0cbf9b3ca07856630f77ab5cd6b63f145e75ab
HartJN <jameshart04@gmail.com>
2023-02-06 09:54:46 +1300

    add Projects section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Hero from '../components/Hero'
 import About from '@/components/About'
+import Projects from '@/components/Projects'
 
 // import styles from './page.module.css'
 
@@ -10,6 +11,7 @@ export default function Home() {
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
